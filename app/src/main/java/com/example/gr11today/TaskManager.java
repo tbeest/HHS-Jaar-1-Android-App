d3bc90ecde14d86b4df100c8df6c257dea0bf302
Jordy Laptop <jordy_donker@hotmail.com>
2022-06-09 09:21:43 +0200

    Creating new activity taskmanager

---
+++ app/src/main/java/com/example/gr11today/TaskManager.java
@@ -0,0 +1,14 @@
+package com.example.gr11today;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_manager);
+    }
+}
