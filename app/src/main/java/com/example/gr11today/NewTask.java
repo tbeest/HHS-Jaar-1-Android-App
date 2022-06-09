879aa7fd2a6badcf4c0ba915b3b5cafeb07f6825
Jordy Laptop <jordy_donker@hotmail.com>
2022-06-09 17:47:55 +0200

    Created new task activity and added strings to xml

---
+++ app/src/main/java/com/example/gr11today/NewTask.java
@@ -0,0 +1,14 @@
+package com.example.gr11today;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
