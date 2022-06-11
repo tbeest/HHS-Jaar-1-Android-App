7a546b215aa926882d182492fe7d23401dc5b866
Tijn van der Helm <tijnvanderhelm@gmail.com>
2022-06-11 19:03:09 +0200

    Removed unused activities Updated activity_open_tasks.xml Added relevant strings for activity_open_tasks.xml Added empty directories for models and adapters

--- app/src/main/java/com/example/gr11today/OpenTasks.java
+++ app/src/main/java/com/example/gr11today/OpenTasks.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NewTask extends AppCompatActivity {
+public class OpenTasks extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_task);
+        setContentView(R.layout.activity_open_tasks);
     }
 }
