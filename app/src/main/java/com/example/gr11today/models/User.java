46eb8d9017ceb50688913aa3e6cce5a4fee2ea9f
Jordy Laptop <jordy_donker@hotmail.com>
2022-06-14 20:36:59 +0200

    Working on UserDao

--- app/src/main/java/com/example/gr11today/models/User.java
+++ app/src/main/java/com/example/gr11today/models/User.java
@@ -1,9 +1,15 @@
 package com.example.gr11today.models;
 
+import android.content.Context;
+
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 import androidx.room.ColumnInfo;
+
+import com.example.gr11today.Database;
+
+import java.util.List;
 
 @Entity(tableName = "users")
 public class User {
@@ -38,6 +44,10 @@ public class User {
                 ", password='" + password + '\'' +
                 '}';
     }
+
+  /*  public static List<User> getAll(Context context) {
+        return Database.getDatabase(context).userDao().getAll();
+    }*/
 
     public Integer getId() {
         return id;
