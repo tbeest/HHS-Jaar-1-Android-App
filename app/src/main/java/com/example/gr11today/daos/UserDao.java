0835a4a6359cf6ff284dabc1532aa5f36c7180b2
Jordy Laptop <jordy_donker@hotmail.com>
2022-06-17 23:17:21 +0200

    Removed unused code.

--- app/src/main/java/com/example/gr11today/daos/UserDao.java
+++ app/src/main/java/com/example/gr11today/daos/UserDao.java
@@ -1,10 +1,8 @@
 package com.example.gr11today.daos;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
 
 import com.example.gr11today.models.User;
 
@@ -17,18 +15,6 @@ public interface UserDao {
     @Query("SELECT * FROM user WHERE name = :username")
     User getByUsername(String username);
 
-/*    @Query("SELECT * FROM task WHERE userId = :userId")
-    List<Task> getAllTasksByUserId(int userId);*/
-
-    @Insert
-    void registerUser(User user);
-
     @Insert
     void insert(User user);
-
-    @Update
-    void Update(User user);
-
-    @Delete
-    void delete(User user);
 }
